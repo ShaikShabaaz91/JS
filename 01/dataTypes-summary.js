bfb5ea00996eec70623fd4747356851cf72630dd
shaikshabazz <41590331+ShaikShabaaz91@users.noreply.github.com>
2024-01-12 19:52:09 +0000

    data rype

---
+++ 01/dataTypes-summary.js
@@ -0,0 +1,13 @@
+//  primitive data types 7 types
+
+// all primitive data types are call by value
+// String,number,null,boolean,undefined,symbol,BigInt
+
+// non-primitive data type are reference type
+
+// array, objects, Functions
+ 
+
+// is javascript static type or dynamic type language 
+// Answer : javascript is a dynamic type language
+
