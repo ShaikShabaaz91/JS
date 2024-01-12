295f547980ae124c068d1b64a35c49d09777de35
shaikshabazz <41590331+ShaikShabaaz91@users.noreply.github.com>
2024-01-12 18:55:08 +0000

    data-types

---
+++ 01/01-variables.js
@@ -0,0 +1,13 @@
+const account_id = 1122
+let account_email = "shaikshabazz111@gmail.com"
+var account_password = '1234'
+account_city = 'Bengaluru'
+let accountType;
+
+
+// account_id = 2
+account_email = "shabazz@gmail.com"
+account_password = 143
+
+console.log("<<>>>", account_id);
+console.table([account_email, account_id, account_password,accountType]);
---
+++ 01/02-data-types.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using NodeJs Not browser
+
+// String
+// null
+// undefined
+// bigInt
+// Boolean
+
+
+console.log(typeof null) //object 
