65643520fbf3b020accfaea7d807ed921a84b622
shaikshabazz <41590331+ShaikShabaaz91@users.noreply.github.com>
2024-01-12 19:07:57 +0000

    conversion of data types

---
+++ 01/03-conversion-operations.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+console.log(typeof score);
+
+let value = Number("30");
+
+console.log("value", value);
+
+// "33" => 33
+// "33abc" => NAN
+// "true" => 1
+
+let Loginedin = 1;
+
+let BooleanLoginedIn = Boolean(Loginedin);
+
+console.log("boolean", BooleanLoginedIn);
+
+// 1 => true
+// 2 => fasle
+// "" => fasle
+// "shabazz" => true
+
+let someNumber = 30;
+
+let stringNumber = String(someNumber);
+
+console.log("Number", stringNumber);
+console.log(typeof stringNumber);
