5b6c5a5ec13f44314ddabc02607fd0cb5636ab4a
shaikshabazz <41590331+ShaikShabaaz91@users.noreply.github.com>
2024-01-13 09:33:18 +0000

    nums

---
+++ 01/06-nums-math.js
@@ -0,0 +1,28 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 12.389;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++ Maths +++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,2,3,1))
+// console.log(Math.max(4,5,2,1));
+
+console.log(Math.random());
+
