edd90928025aecbd31ad3dd3f3090d904c05370b
shaikshabazz <41590331+ShaikShabaaz91@users.noreply.github.com>
2024-01-13 09:49:10 +0000

    date

---
+++ 01/07-date.js
@@ -0,0 +1,11 @@
+const date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(date.toString());
+
+const new_date = new Date(2024, 0, 13);
+console.log(new_date.toDateString());
+
+const date_time = new Date(2024, 1, 13, 4, 2);
+console.log(date_time.toLocaleString());
